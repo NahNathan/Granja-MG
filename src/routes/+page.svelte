4f8b320b43e5ae50b05d5891e214c4ca1f20702e
NahNathan <nathanrodriguessantos3@gmail.com>
2025-02-07 10:53:54 -0300

    Config de banco inicial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,24 @@
 <script>
+  import { onMount } from 'svelte';
+
   /**
 	 * @type {any[]}
 	 */
   let ovos = [];
 
-  // Busca os dados do endpoint
-  async function carregarOvos() {
+  // Use onMount para rodar fetch somente no cliente
+  onMount(async () => {
     const response = await fetch('/api/ovos');
     ovos = await response.json();
-    console.log(ovos)
-  }
-
-  carregarOvos();
+    console.log(ovos);
+  });
 </script>
 
 <h1>Dashboard do Sistema</h1>
 <p>Bem-vindo ao sistema de gerenciamento de ovos e animais!</p>
 
+<ul>
+  {#each ovos as ovo}
+    <li>{ovo.tipoAnimal}: {ovo.guardados} guardados</li>
+  {/each}
+</ul>
